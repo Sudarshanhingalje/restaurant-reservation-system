14d060f190b5136032f131e9205834eb4891c58d
Sudarshan kalgonda hingalje <47637833+Sudarshanhingalje@users.noreply.github.com>
2025-01-01 00:57:49 +0530

    Update ForReservation.jsx

--- frontEnd/src/components/ForReservation.jsx
+++ frontEnd/src/components/ForReservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:2321/api/v1/reservation/send",
+        "https://restaurant-reservation-system-qzxm.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
